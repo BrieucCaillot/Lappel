2e5907708c4b7a2f4c3e268857bb70815006d122
Karl <karljustinianodev@gmail.com>
2020-10-26 16:04:42 +0100

    Implemented camera transition system

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CameraManager : MonoBehaviour {
+    public CinemachineVirtualCamera cam;
+    public PlayableDirector timeline;
+    public PlayableDirector timeline2;
+
+    private void Start() {
+
+        // StartCoroutine("StartTimeline");
+    }
+
+    public void StartTimeline(string Movement) {
+        switch (Movement) {
+            case "cam1":
+                Debug.Log("cam1");
+                timeline.Play();
+                break;
+            case "cam2":
+                Debug.Log("cam2");
+                timeline2.Play();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // IEnumerator StartTimeline(){
+    //     yield return new WaitForSeconds(3f);
+    //     timeline.Play();
+    // }
+
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Camera/MovementScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public GameObject cameraManager;
+    private CameraManager cameraManagerScript;
+
+    void Start() {
+        cameraManagerScript = cameraManager.GetComponent<CameraManager>();
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        switch (collider.tag) {
+            case "CollideTest":
+                Debug.Log("call timeline1 cam1");
+                cameraManagerScript.StartTimeline("cam1");
+                break;
+            case "CollideTest2":
+                Debug.Log("call timeline2 cam2");
+                cameraManagerScript.StartTimeline("cam2");
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("q")) {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("z")) {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s")) {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+    }
+}
