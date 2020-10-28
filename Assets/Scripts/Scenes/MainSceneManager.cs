b3466edb89492965cae214c1827b9ac44ed1e32c
Brieuc Caillot <brieucdeveloper@gmail.com>
2020-10-28 17:02:34 +0100

    Added few assets

--- Assets/Scripts/Scenes/MainSceneManager.cs
+++ Assets/Scripts/Scenes/MainSceneManager.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScenesManager.Instance.LoadScenes("Main Scene Environment", true);
+        ScenesManager.Instance.LoadScenes("Main Scene Core", true);
     }
 
     // Update is called once per frame
