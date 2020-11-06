e55e5882b0bc782c0d4825de9b4eaac3ded06194
BrieucKyo <brieucdeveloper@gmail.com>
2020-11-06 17:11:10 +0100

    Added left foot and sounds

---
+++ Assets/Scripts/Penguins/PenguinFootstepSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinFootstepSpawn : MonoBehaviour
+{
+    public GameObject rightFootprint;
+    public GameObject leftFootprint;
+
+    public Transform leftFootLocation;
+    public Transform rightFootLocation;
+    
+    public float footPrintOffset = 0.05f;
+    
+    public AudioSource[] audioSources;
+    
+    public void LeftFootstep()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(leftFootLocation.position, leftFootLocation.forward, out hit))
+        {
+            Instantiate(leftFootprint, hit.point + hit.normal * footPrintOffset, Quaternion.LookRotation(hit.normal, leftFootLocation.up));
+        }
+    }
+    public void RightFootstep()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(rightFootLocation.position, rightFootLocation.forward, out hit))
+        {
+            audioSources[Random.Range(0, audioSources.Length - 1)].Play();
+            Instantiate(rightFootprint, hit.point + hit.normal * footPrintOffset, Quaternion.LookRotation(hit.normal, rightFootLocation.up));
+        }
+    }
+}
